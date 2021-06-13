bd63f4c78e8fb140d4bcb98c2654222d26fe491d
Gerardo Rivera Tello <gerardo_art@me.com>
2021-06-12 19:35:09 -0500

    Start writing some tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,20 +1,17 @@
 import numpy as np
-import pytest
+import pandas as pd
+import xarray as xr
 
 import xenso
 
 
-@pytest.mark.web
-def test_meaning_of_life_url():
-    ret = xenso.meaning_of_life_url()
+def test_compute_climatology():
+    dates = pd.date_range("1981-01-01", "2010-12-31", freq="M")
+    data = xr.DataArray(np.tile(np.arange(12), 30), coords=[("time", dates)])
 
-    assert isinstance(ret, str)
-    assert "Monty Python" in ret
-    assert "Meaning of Life" in ret
+    result = xenso.compute_climatology(data)
+    expected = xr.DataArray(np.arange(12), coords=[("month", np.arange(1, 13))])
+    xr.testing.assert_equal(result, expected)
 
-
-def test_meaning_of_life():
-    n = 2
-    ret = xenso.meaning_of_life(n)
-    assert isinstance(ret, np.ndarray)
-    assert np.unique(ret) == 42
+    result = xenso.compute_climatology(data, ("2000-01-01", "2006-12-31"))
+    xr.testing.assert_equal(result, expected)
