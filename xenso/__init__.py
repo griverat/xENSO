ee1fd6ee474904643e584f4cdad8d852d09ba0a1
DangoMelon <gerardo_art@me.com>
2023-08-22 00:05:02 -1000

    Add indices submodule as module export

--- xenso/__init__.py
+++ xenso/__init__.py
@@ -2,9 +2,10 @@
 ioos_pkg_skeleton is not a real package, just a set of best practices examples.
 """
 
+from . import indices
 from .core import compute_anomaly, compute_climatology, xconvolve
 
-__all__ = ["compute_climatology", "compute_anomaly", "xconvolve"]
+__all__ = ["compute_climatology", "compute_anomaly", "xconvolve", "indices"]
 
 try:
     from ._version import __version__
